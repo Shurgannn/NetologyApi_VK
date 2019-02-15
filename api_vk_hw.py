a7f8ece6b70af11572b95afb025c3a1078d360aa
Shurgannn <46814630+Shurgannn@users.noreply.github.com>
2019-02-15 23:21:24 +0300

    Add files via upload

--- api_vk_hw.py
+++ api_vk_hw.py
@@ -11,24 +11,14 @@ auth_data = {
     'response_type': 'token',
     'v': '5.92',
 }
-
 #print('?'.join((AUTH_URL, urlencode(auth_data))))
-
 TOKEN = '9fe89e091c0ddea40f26dd811ba3160e16b7241e31c4f9e30446299c4d339e5a3455111220dbedfed9774'
-
-#params = {
-#    'v': '5.92',
-#    'access_token': TOKEN,
-#    'text': '!'
-#}
-#response = requests.get('https://api.vk.com/method/status.set', params)
-#pprint(response.json())
 
 
 class User:
 
-    def __init__(self, token):
-        self.token = token
+    def __init__(self, id):
+        self.id = id
 
     def get_params(self):
         return {
@@ -36,20 +26,12 @@ class User:
             'access_token': TOKEN
         }
 
-   # def friends_id(self):
-    #    params = self.get_params()
-     #   params['fields'] = 'o'
-      #  response = requests.get('https://api.vk.com/method/friends.get', params)
-       # pprint(response.json())
-        #return response.json()
-
-    def friends(self):
+    def friends_id(self):
         params = self.get_params()
-        response = requests.get('https://api.vk.com/method/users.get', params)
+        params['fields'] = 'first_name'
+        response = requests.get('https://api.vk.com/method/friends.get', params)
         pprint(response.json())
         return response.json()
-
-
 
     def get_friends(self, uid):
         params = self.get_params()
@@ -60,9 +42,9 @@ class User:
 
 
 
-user = User(TOKEN)
+user = User(id)
 #user.get_status()
 #user.set_status('?')
 #user.get_status()
 user.get_friends(45567)
-user.friends()
+user.friends_id()
