b4d695469f5695497841327aae1727b9a633ba33
Shurgannn <46814630+Shurgannn@users.noreply.github.com>
2019-02-25 14:29:01 +0300

    Add files via upload

--- diploma.py
+++ diploma.py
@@ -1,6 +1,7 @@
 import requests
 from pprint import pprint
 import time
+import json
 
 user_name = 'eshmargunov'
 ID = 171691064
@@ -14,17 +15,15 @@ params = {
     'v': '5.92',
     'access_token': TOKEN,
     'user_id': ID,
-    # 'screen_name': user_name
+    'user_ids': user_name,
 }
 
 
 def get_groups():  # группы пользователя
     params['extended'] = 1
     params['fields'] = 'members_count'
-    resp_js_gr = requests.get('https://api.vk.com/method/groups.get', params).json()
-    # for group in resp_js_gr:
-    #     print(group["name"])
-    return resp_js_gr['response']['items']
+    resp_js_gr = requests.get('https://api.vk.com/method/groups.get', params).json()['response']['items']
+    return resp_js_gr
 
 
 def get_friends():  # друзья пользователя
@@ -40,15 +39,19 @@ def friends_groups():  # группы друзей
         print('-', c)
         params['user_id'] = friends['id']
         response = requests.get('https://api.vk.com/method/groups.get', params)
-        print(response.json())
+       # print(response.json())
         try:
             resp_js = response.json()['response']['items']
-            print(resp_js)
+           # print(resp_js)
             friends_grouplist.append(resp_js)
+            '/n'
         except KeyError:
             if response.json()['error']['error_code'] == 6:
-                time.sleep(0.6)
-            print('Ошибка 7. Нет прав для выполнения этого действия.')
+                time.sleep(1)
+            elif response.json()['error']['error_code'] == 7:
+                print('Ошибка 7. Нет прав для выполнения этого действия.')
+            elif response.json()['error']['error_code'] == 18:
+                print('Ошибка 18. Страница удалена или заблокирована.')
     return friends_grouplist
 
 
@@ -58,17 +61,28 @@ def answer_for_d():
     user_friends_groups = friends_groups()
     # pprint(user_friends_groups)
     i = 0
+    list_answers_for_d = []
     while i < len(user_groups):
         for group in user_friends_groups:
             if user_groups[i] in group:
                 break
         else:
-            with open('groups.json', 'a', encoding='utf-8') as fa:
-                fa.write(str(user_groups[i]) + '\n')
+            list_answers_for_d.append(user_groups[i])
         i += 1
+    for group in list_answers_for_d:
+        del group['screen_name']
+        del group['is_closed']
+        del group['photo_100']
+        del group['photo_200']
+        del group['photo_50']
+        del group['type']
+    return list_answers_for_d
 
 
-# pprint(get_groups())
+with open('groups.json', 'w', encoding='utf-8') as fw:
+    json.dump(answer_for_d(), fw)
+
+# print(get_groups())
 # pprint(get_friends())
 # pprint(friends_groups())
-answer_for_d()
+# answer_for_d()
