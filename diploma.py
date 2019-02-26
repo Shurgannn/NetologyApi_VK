53bf588922d3614b2bf88faee32ff740d373ec1c
Shurgannn <46814630+Shurgannn@users.noreply.github.com>
2019-02-26 10:30:45 +0300

    Add files via upload

--- diploma.py
+++ diploma.py
@@ -3,8 +3,9 @@ from pprint import pprint
 import time
 import json
 
-user_name = 'eshmargunov'
-ID = 171691064
+# user_name = input('Введите имя пользователя')# 'eshmargunov'
+# ID = input('Введите id пользователя')# 171691064
+users = input('Введите id или имя пользователя')
 
 with open('take_token.txt', encoding='utf-8') as f:
     TOKEN = f.read()
@@ -14,8 +15,8 @@ with open('take_token.txt', encoding='utf-8') as f:
 params = {
     'v': '5.92',
     'access_token': TOKEN,
-    'user_id': ID,
-    'user_ids': user_name,
+    # 'user_id': ID,
+    'user_ids': users,
 }
 
 
@@ -33,25 +34,30 @@ def get_friends():  # друзья пользователя
 
 def friends_groups():  # группы друзей
     c = 0
+    c2 = 0
     friends_grouplist = []
     for friends in get_friends():
         c += 1
         print('-', c)
-        params['user_id'] = friends['id']
-        response = requests.get('https://api.vk.com/method/groups.get', params)
-       # print(response.json())
-        try:
-            resp_js = response.json()['response']['items']
-           # print(resp_js)
-            friends_grouplist.append(resp_js)
-            '/n'
-        except KeyError:
-            if response.json()['error']['error_code'] == 6:
-                time.sleep(1)
-            elif response.json()['error']['error_code'] == 7:
-                print('Ошибка 7. Нет прав для выполнения этого действия.')
-            elif response.json()['error']['error_code'] == 18:
-                print('Ошибка 18. Страница удалена или заблокирована.')
+        while c2 < c:
+            params['user_id'] = friends['id']
+            response = requests.get('https://api.vk.com/method/groups.get', params)
+            try:
+                resp_js = response.json()['response']['items']
+                print(resp_js)
+                friends_grouplist.append(resp_js)
+                c2 += 1
+            except KeyError:
+                if response.json()['error']['error_code'] == 6:
+                    print('Ошибка 6.')
+                    time.sleep(0.8)
+                elif response.json()['error']['error_code'] == 7:
+                    print('Ошибка 7. Нет прав для выполнения этого действия.')
+                    c2 += 1
+                elif response.json()['error']['error_code'] == 18:
+                    print('Ошибка 18. Страница удалена или заблокирована.')
+                    c2 += 1
+        print(c2)
     return friends_grouplist
 
 
@@ -67,15 +73,16 @@ def answer_for_d():
             if user_groups[i] in group:
                 break
         else:
-            list_answers_for_d.append(user_groups[i])
+            pprint(user_groups[i])
+            list_answers_for_d.append((user_groups[i])['name'])
         i += 1
-    for group in list_answers_for_d:
-        del group['screen_name']
-        del group['is_closed']
-        del group['photo_100']
-        del group['photo_200']
-        del group['photo_50']
-        del group['type']
+    #for group in list_answers_for_d:
+    #    del group['screen_name']
+     #   del group['is_closed']
+     #   del group['photo_100']
+     #   del group['photo_200']
+     #   del group['photo_50']
+     #   del group['type']
     return list_answers_for_d
 
 
